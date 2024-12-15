7847c89b360cff6d2da7476087725d857038472e
IsraelIyke <nwangwuisrael@gmail.com>
2024-12-15 10:08:55 +0100

    lastest version of the v-scrapper

--- download_pdf.py
+++ download_pdf.py
@@ -26,33 +26,33 @@ filtered_list = [
 ]
 
 # filtered_list = filtered_list[50:]
-print(len(filtered_list))
+# print(len(filtered_list))
 
 
-# # Process each value in the filtered list
-# for value in filtered_list:
-#     pyautogui.sleep(5)
-#     # These coordinates are based on my PC resolution. Please adjust to your own system
-#     pyautogui.click(1216, 1026)
-#     pyautogui.sleep(1)
-#     pyautogui.write(value)
-#     pyautogui.click(1665, 1170)
-#     pyautogui.sleep(30)
+# Process each value in the filtered list
+for value in filtered_list:
+    pyautogui.sleep(5)
+    # These coordinates are based on my PC resolution. Please adjust to your own system
+    pyautogui.click(1216, 1026)
+    pyautogui.sleep(1)
+    pyautogui.write(value)
+    pyautogui.click(1665, 1170)
+    pyautogui.sleep(30)
     
-#     pyautogui.click(828, 1366)
-#     pyautogui.sleep(4)
-#     pyautogui.click(2003, 1318)
-#     pyautogui.sleep(2)
-#     pyautogui.write(value)
-#     pyautogui.sleep(3)
-#     pyautogui.click(905, 909)
-#     pyautogui.sleep(2)
+    pyautogui.click(828, 1366)
+    pyautogui.sleep(4)
+    pyautogui.click(2003, 1318)
+    pyautogui.sleep(2)
+    pyautogui.write(value)
+    pyautogui.sleep(3)
+    pyautogui.click(905, 909)
+    pyautogui.sleep(2)
 
-#     pyautogui.press('enter')
-#     pyautogui.press('esc')
-#     pyautogui.press('esc')
+    pyautogui.press('enter')
+    pyautogui.press('esc')
+    pyautogui.press('esc')
 
-#     pyautogui.sleep(2)
-#     pyautogui.click(1000, 186)
-#     pyautogui.write("https://unnportal.unn.edu.ng/putme_login")
-#     pyautogui.press('enter')
+    pyautogui.sleep(2)
+    pyautogui.click(1000, 186)
+    pyautogui.write("https://unnportal.unn.edu.ng/putme_login")
+    pyautogui.press('enter')
